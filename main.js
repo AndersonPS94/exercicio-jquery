5f81745b533ddbfda9a182316c19a5b22c6df81e
AndersonPS94 <dson_adn@hotmail.com>
2023-09-14 21:11:33 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    
+    $("#form-tarefa").submit(function(e) {
+      e.preventDefault();
+
+      const Tarefa = $("#adiciona-tarefa").val();
+      const novaTarefa = $("#lista-tarefa").append("<li>" + Tarefa + "</li>");
+      $("#adiciona-tarefa").val("");
+    });
+
+
+    $("#tarefa-li").on("click", "li", function() {
+      $(this).toggleClass('line-through');
+    });
+
+});
